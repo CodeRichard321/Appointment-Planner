54a1775042c06bfa4eded6161800275b3ba07f6d
CodeRichard321 <68620723+CodeRichard321@users.noreply.github.com>
2022-05-30 18:36:42 -0500

    Update

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (obj) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{obj[0]}</p>
+      {obj.values().forEach(prop => <p className="tile">{prop}</p>)}
     </div>
   );
 };
